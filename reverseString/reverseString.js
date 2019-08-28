a06da16f89cfabe5a4aac1b13e2d963dbef4b25f
Vojtech Bartos <vojtabar119@gmail.com>
2019-08-28 22:37:47 +0200

    finish reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitStr=str.split("");
+    let reverseSplitStr=splitStr.reverse();
+    let reverseStr=reverseSplitStr.join("");
+    return reverseStr;
 }
 
 module.exports = reverseString
