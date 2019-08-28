f5d7fbef03d6174562a813391e8ecbe5b54a7efb
Vojtech Bartos <vojtabar119@gmail.com>
2019-08-28 22:27:15 +0200

    finish repeatString.js

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let together="";
+    if (num<0){
+        together="ERROR";
+    }
+    for (let i=0;i<num;i++){
+        together+=str;
+    }
+    return together;
 }
 
 module.exports = repeatString
