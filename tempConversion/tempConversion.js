41a140468b87964ba0a90c31e849f5e92a518301
Vojtech Bartos <vojtabar119@gmail.com>
2019-08-29 15:46:12 +0200

    finish tempConversion.js

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(ftocInput) {
+  ftocOutput = (ftocInput-32)*(5/9);
+  ftocOutput = Math.round(ftocOutput * 10) / 10
+  return ftocOutput;
 }
 
-const ctof = function() {
-
+const ctof = function(ctofInput) {
+  ctofOutput = ctofInput*(9/5)+32;
+  ctofOutput = Math.round(ctofOutput * 10) / 10
+  return ctofOutput;
 }
 
 module.exports = {
