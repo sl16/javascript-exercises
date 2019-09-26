3f567781051d24cbb1e419db6818feb70dcac8ad
sl16 <vojtabar119@gmail.com>
2019-09-26 23:53:26 +0200

    finish calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,34 @@
-function add () {
+function add(a,b) {
+	return a+b;
 	
 }
 
-function subtract () {
-	
+function subtract(a,b) {
+	return a-b;
 }
 
-function sum () {
-	
+function sum(array) {
+	return array.reduce(function(a,b){
+		return a + b
+	  }, 0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	return array.reduce(function(a,b){
+		return a * b
+	  });
 }
 
-function power() {
-	
+function power(a,b) {
+	return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(a) {
+	let fact = 1;
+	for(a; a>0;a--){
+		fact*=a;
+	}
+	return fact;
 }
 
 module.exports = {
