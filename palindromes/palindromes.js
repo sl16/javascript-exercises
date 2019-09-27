bb8b231ebd0d53996c1fcdef3b29372c50dcdfbd
sl16 <vojtabar119@gmail.com>
2019-09-27 21:57:44 +0200

    finish palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+    let normalStr = str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
+    let arr = normalStr.split("");
+    let reverseArr  = arr.slice().reverse();
+    let reverseStr = reverseArr.join("");
+    if (reverseStr==normalStr){
+        return true;
+    } else{
+        return false;
+    }
 }
 
 module.exports = palindromes
