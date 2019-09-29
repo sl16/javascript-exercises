f2edb7f7fff90958b1c967b2879719711dcfeb1b
sl16 <vojtabar119@gmail.com>
2019-09-29 22:56:34 +0200

    finish fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(specMember) {
+    if (specMember<0){
+        return "OOPS";
+    }
+    let sequence = [1,1];
+    for (let i = 2;i < specMember; i++){
+        sequence.push(sequence[sequence.length-2]+sequence[sequence.length-1]);
+    }
+    return sequence[specMember-1];
 }
 
 module.exports = fibonacci
