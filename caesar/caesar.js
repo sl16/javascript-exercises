4bef244c4bc7268b4a78cee67e0554804fc086d3
sl16 <vojtabar119@gmail.com>
2019-09-29 22:38:35 +0200

    finish caesar.js

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,26 @@
-const caesar = function() {
-
+const caesar = function(msg, shiftBy) {
+    let arr = Array.from(msg);
+    // IF SHIFT IS NEGATIVE CONVERT INTO POSITIVE
+    if (shiftBy<0){
+        shiftBy = 26 + (shiftBy % 26);
+    }
+    for (let i = 0; i < arr.length; i++){
+        // IF ARAY ITEM IS A LETTER SHIFT IT
+        if (arr[i].toUpperCase() != arr[i].toLowerCase()){
+            // LOWER CASE LETTERS ONLY LOOP IN LOWER CASE
+            if (arr[i] == arr[i].toLowerCase()){
+                arr[i] = (((arr[i].charCodeAt(0) - 97 + shiftBy) %26 ) + 97);
+            // UPPER CASE LETTERS ONLY LOOP IN UPPER CASE
+            } else if (arr[i] == arr[i].toUpperCase()){
+                arr[i] = (((arr[i].charCodeAt(0) - 65 + shiftBy) %26 ) + 65);
+            }
+            // TRANSFORM BACK INTO LETTER
+            arr[i] = String.fromCharCode(arr[i]);
+        // IF ARRAY ITEM NOT LETTER CONTINUE
+        } else continue;
+    }
+    // RETURN JOINED STRING
+    return arr.join("");
 }
 
 module.exports = caesar
